5a72606156f21dd0cf1268f252ee6382a9f204b7
jvitX <jvitfaria@gmail.com>
2026-07-28 13:57:29 -0300

    JAO MUDOU

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF6F7FF), // cor de fundo
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
